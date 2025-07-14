83e88199338cda40d73abaca7ca1bb1338f7641e
parusha <parusha.pradhan@gmail.com>
2025-07-13 20:06:25 -0400

    clean up

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -27,12 +27,7 @@ export default async function ProductsPage(
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-end gap-2">
-        <Button size="sm" variant="outline" className="h-8 gap-1">
-          <File className="h-3.5 w-3.5" />
-          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-            Export
-          </span>
-        </Button>
+    
         <AddProductForm />
       </div>
       <ProductsTabs
--- app/(dashboard)/user.tsx
+++ app/(dashboard)/user.tsx
@@ -51,7 +51,7 @@ export async function User() {
           </DropdownMenuItem>
         ) : (
           <DropdownMenuItem>
-            <Link href="/login">Sign In</Link>
+            <Link href="#">Sign In</Link>
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
